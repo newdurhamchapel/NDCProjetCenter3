6fa048b94bca4872870ae9b7774d8edefbebf8d4
codedev@newdurhamchapel.org <codedev@newdurhamchapel.org>
2017-01-22 18:07:12 -0500

    Initial commit

---
+++ app/src/main/java/org/newdurhamchapel/ndcprojetcenter/MainActivity.java
@@ -0,0 +1,18 @@
+package org.newdurhamchapel.ndcprojetcenter;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView)findViewById(R.id.tvmain);
+
+        tv.setText("Testing View!");
+    }
+}
